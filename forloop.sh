c722d44b04e3b35f23973fae837388171d01f627
Claude Eke <visionary_p@yahoo.com>
2023-08-20 00:44:18 -0700

    Ticket solved

--- forloop.sh
+++ forloop.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 # Write a shell script to print numbers from 100 to 90 using for loop.
+# Author: Claude Eke
+echo "Printing of numbers commencing"
+for (( x=100; x>=90; x-- )); do
+	echo $x
+done
+echo "Printing of numbers done"
